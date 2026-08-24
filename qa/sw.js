b6e7aaca5f31f0a10ade1d1c3184a9fa96af9850
xmarmijo <miguel.armijo.n@gmail.com>
2026-08-23 22:17:54 -0400

    QA: miConfirmar renderiza HTML, cache qa-v2

--- qa/sw.js
+++ qa/sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'mis-huevos-qa-v1';
+﻿const CACHE_NAME = 'mis-huevos-qa-v2';
 const ASSETS = [
   './',
   './MisHuevos_Movil.html',
@@ -37,6 +37,7 @@ self.addEventListener('fetch', e => {
     })
   );
 });
+
 
 
 
